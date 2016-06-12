a9d750517bd0aaaf82d0f51b5ba584744893545c
faroreJP <creator.farore@gmail.com>
2016-06-12 20:14:27 +0900

    add entry point

---
+++ src/main/java/farore/mfmf/Mofu.java
@@ -0,0 +1,19 @@
+package farore.mfmf;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+/**
+ * Created by farore on 16/06/12.
+ */
+@Mod(modid = Mofu.MODID, version = Mofu.VERSION)
+public class Mofu {
+    public static final String MODID = "Mofu";
+    public static final String VERSION = "0.0.1";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+}
